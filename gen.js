83e75b060384fee81b7633695ec0722e27096d9f
3bl3gamer <3bl3gamer@gmail.com>
2016-05-14 12:35:09 +0300

    Special 'as is' files group

--- gen.js
+++ gen.js
@@ -12,6 +12,11 @@ const {getMarkedConfig} = require('./marked_config.js')
 const OUT_DIR = 'www'
 const PAGES_DIR = 'pages'
 const MAIN_LANGSERVER = 'ru-srv'
+const COPY_AS_IS = ['CNAME', 'static', 'robots.txt'].map(name => PAGES_DIR+'/'+name)
+
+function withExt(name, ext) {
+	return ext == null ? name : name+'.'+ext
+}
 
 // INIT
 const markedConfig = getMarkedConfig(marked)
@@ -56,16 +61,17 @@ forEachFile(PAGES_DIR, (fname, fullpath) => {
 	if (fullpath.endsWith('~')) return //бекапы текстового редактора, пропускаем
 
 	// index.ru-server.html, название.язык-сервер.формат
-	let m = fullpath.match(/^([\w/]*)\/([^.]*)(?:\.([a-z]{2})-(\w+))?\.(\w+)$/)
+	let m = fullpath.match(/^([\w/]*)\/([^.]*)(?:\.([a-z]{2})-(\w+))?(?:\.(\w+))?$/)
 	if (!m) { console.warn(`${fullpath} has wrong name/path, skipping`); return }
 
 	let [_, path, name, lang, server, ext] = m
 	path = path.substr(PAGES_DIR.length+1) // отрезаем префикс `pages/` от пути
 	lang = lang || '*'
 	server = server || '*'
+	let copy_as_is = COPY_AS_IS.some( prefix => fullpath.startsWith(prefix) )
 
 	files.push({
-		lang, server, path, name, ext,
+		lang, server, path, name, ext, copy_as_is,
 		filepath: fullpath
 	})
 })
@@ -89,7 +95,8 @@ console.log('')
 // Всяких `index.*-srv.html` в `index.ru-srv.html`, `index.en-srv.html` и т.д.
 console.log('duplicating pages...')
 for (let i=0; i<files.length; i++) {
-	let {lang, server, path, name, ext, filepath} = files[i]
+	let {lang, server, path, name, ext, filepath, copy_as_is} = files[i]
+	if (copy_as_is) continue
 	if (lang != '*' && server != '*') continue
 
 	// убираем сам файл...
@@ -126,7 +133,7 @@ console.log(`  done, ${files.length} now\n`)
 // }
 let groups = {}
 for (let file of files) {
-	let {lang, server, path} = file
+	let {lang, server, path, copy_as_is} = file
 
 	// пустая страница, заполнится (если есть, чем) по мере обработки файлов
 	function blankPage(name, path) {
@@ -139,7 +146,7 @@ for (let file of files) {
 		}
 	}
 
-	let langserv = lang+'-'+server
+	let langserv = copy_as_is ? '*as-is*' : lang+'-'+server
 	if (!(langserv in groups)) groups[langserv] = blankPage(langserv, '') //{name:langserv, lang, server, files:[], children:{}}
 
 	let cur_page = groups[langserv]
@@ -163,7 +170,7 @@ console.log('pages structure:')
 function iter(page, level) {
 	console.log('- ' + '  '.repeat(level) + page.name + '/')
 	for (let file of page.files) {
-		console.log('- ' + '  '.repeat(level+1) + file.name+'.'+file.ext)
+		console.log('- ' + '  '.repeat(level+1) + withExt(file.name, file.ext))
 	}
 	for (let i in page.children) {
 		iter(page.children[i], level+1)
@@ -186,28 +193,28 @@ function cmp(a,b){ return a==b ? 0 : a<b ? -1 : 1 }
 files.sort((f1, f2) => cmp(f1.lang+'-'+f1.server+'-'+f1.filepath,
                            f2.lang+'-'+f2.server+'-'+f2.filepath))
 
-for (let {server, path, lang, name, ext, filepath} of files) {
+for (let {server, path, lang, name, ext, filepath, copy_as_is} of files) {
 	let sections = path.split('/')
 	let langserver = lang+'-'+server
 	let is_main = langserver == MAIN_LANGSERVER
-	let menu = groups[langserver].children
 	let content = fs.readFileSync(filepath)
 
 	if (ext == 'md' || ext == 'html') content = content.toString()
 
 	if (ext == 'md') content = marked(content, markedConfig)
 
-	let pagepath = is_main ? `/${path}/` : `/${langserver}/${path}/`
-	let outpath = `${OUT_DIR}${pagepath}${name}.${ext=='md'?'html':ext}`
+	let pagepath = is_main || copy_as_is ? `/${path}/` : `/${langserver}/${path}/`
+	let outpath = `${OUT_DIR}${pagepath}${withExt(name, ext=='md'?'html':ext)}`
 
 	if (ext == 'md' || ext == 'html') {
 		let def = makeDef(main_def)
 		content = dot.template(content, null, def)({lang, server, path, pagepath})
+		let menu = groups[langserver].children
 		let html = main({title: sections[sections.length-1], menu, lang, server, path, pagepath, content})
 		content = beautify(html, beautify_cfg)
 	}
 
-	console.log(`  writing ${is_main?'m':' '} ${outpath}...`)
+	console.log(`  writing ${is_main?'m':copy_as_is?'c':' '} ${outpath}...`)
 	write(outpath, content)
 }
 console.log('  done\n')
