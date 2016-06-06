66b8775b05e5faef5cc349951fe14c6675bdc2eb
LookNya <pavelx58@yandex.ru>
2016-06-06 15:09:37 +0300

    еще дизайны

--- pages/static/js/idb/item-cards.js
+++ pages/static/js/idb/item-cards.js
@@ -11,6 +11,8 @@ function ItemCard(item, params){
 	cardEl.id = item.id
 	var maxLvl = Object.keys(item.params).length
 	var lvlSelector = genLevelSelector()
+	var p = item.params[1] || {}
+	console.log(p)
 	cardEl.classList.add('item-card')
 	cardEl.innerHTML = '\
 		<div class="icon" style="background-image: url('+ (item.params[1] ? item.params[1].icon : '') +')"></div>\
@@ -18,18 +20,19 @@ function ItemCard(item, params){
 		<br clear="all">' +
 			lvlSelector
 			+
-			'<div class="descr">'+
 			(item.obtaining ? '<div class="obtaining">'     + stat_namesDB.obt  + ': ' + item.obtaining + '</div>' : '') +
-			(item.acc ?       '<div class="acc f_l">'       + stat_namesDB.acc  + ': <b>' + item.acc       + '</b></div>' : '') +
+			'<div class="descr">'+
+			(p['Сила атаки'] ? '<div class="atk f_l">'  + 'Сила атаки'      + ': <b>' + p['Сила атаки'] + '</b></div>' : '') +
+			(p['HP'] ? '<div class="hp f_l">'  + 'Здоровье'      + ': <b>' + p['HP'] + '</b></div>' : '') +
+			(p['Точность'] ?  '<div class="acc f_l">'   + 'Точность'  + ': <b>' + p['Точность']      + '</b></div>' : '') +
 			(item.atp ?       '<div class="atp f_l">'       + stat_namesDB.atp  + ': <b>' + item.atp       + '</b></div>' : '') +
-			(item.atk ?       '<div class="atk f_l">'       + stat_namesDB.atk  + ': <b>' + item.atk       + '</b></div>' : '') +
 			(item.def ?       '<div class="def f_l">'       + stat_namesDB.def  + ': <b>' + item.def       + '</b></div>' : '') +
 			(item.crit ?      '<div class="crit f_l">'      + stat_namesDB.crit  + ': <b>' + item.crit     + '</b></div>' : '') +
 			(item.cdef ?      '<div class="cdef f_l">'      + stat_namesDB.cdef  + ': <b>' + item.cdef     + '</b></div>' : '') +
 			(item.crithd ?    '<div class="crithd f_l">'    + stat_namesDB.crithd + ': <b>' + item.crithd  + '</b></div>' : '') +
-			(item.prc ?       '<div class="prc f_l">'       + stat_namesDB.prc   + ': <b>' + item.prc      + '</b></div>' : '') +
-			(item.bl ?        '<div class="bl f_l">'        + stat_namesDB.bl    + ': <b>' + item.bl       + '</b></div>' : '') +
-			(item.ev ?        '<div class="ev f_l">'        + stat_namesDB.ev    + ': <b>' + item.ev       + '</b></div>' : '') +
+			(p['Пробивание'] ?       '<div class="prc f_l">'       + 'Пробивание'   + ': <b>' + p['Пробивание']      + '</b></div>' : '') +
+			(p['Блок'] ?        '<div class="bl f_l">'        + 'Блок'    + ': <b>' + p['Блок']       + '</b></div>' : '') +
+			(p['Уклонение'] ? '<div class="ev f_l">'    + 'Уклонение'        + ': <b>' + p['Уклонение'] + '</b></div>' : '') +
 		'</div>\
 		<br clear="all">\
 	'
