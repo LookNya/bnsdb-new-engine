02a15721d5ec5c4d6995bd70a5292c71fb1f66ba
3bl3gamer <3bl3gamer@gmail.com>
2016-10-24 11:26:24 +0300

    Classes, etc

--- bopae_react/src/BopaeList.js
+++ bopae_react/src/BopaeList.js
@@ -1,45 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './styles/bopae-list.css'
 
-var BopaeList = React.createClass({
-	getInitialState: function() {
-			return {
-					selectedItem: null
-			}
-	},
-	clickHandler: function(i) {
-			this.setState({selectedItem: i})
-	},
-	render: function() {
-		var items = this.props.db.map(function(item, i){
-				var is_selected = this.state.selectedItem === i
-				return <ListItem
-								key={i}
-								name={item.name}
-								icon={item.icon}
-								onClick={this.clickHandler.bind(this, i)}
-								isSelected={is_selected}
-								/>
-			}.bind(this)
-		)
+class BopaeList extends Component {
+	constructor() {
+		super()
+		this.state = { selectedItem: null }
+	}
 
+	clickHandler(i) {
+		this.setState({selectedItem: i})
+	}
+
+	render() {
 		return  <div className="bopae-list">
-							{items}
+							{this.props.db.map((item, i) =>
+								<ListItem
+									key={i}
+									name={item.name}
+									icon={item.icon}
+									onClick={this.clickHandler.bind(this, i)}
+									isSelected={this.state.selectedItem === i}
+								/>
+							)}
 						</div>
 	}
-})
-var ListItem = React.createClass({
-	render: function() {
-		var style = {
-				background: 'white'
-		}
-		if (this.props.isSelected) {
-				style['background'] = 'whitesmoke';
+}
+
+class ListItem extends Component {
+	render() {
+		let style = {
+				background: (this.props.isSelected ? 'whitesmoke' : 'white')
 		}
 		return <div style={style} onClick={this.props.onClick}>
 							<img src={this.props.icon} alt="icon"></img>
 							{this.props.name}
 						</div>
 	}
-})
+}
+
 export default BopaeList
