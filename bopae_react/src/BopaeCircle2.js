50f3b5abb1f3acec41cc783e5751630c4065952f
3bl3gamer <3bl3gamer@gmail.com>
2016-10-29 02:31:28 +0300

    Bopae circle version with single image (WORKS, UNBELIEVABLE)

---
+++ bopae_react/src/BopaeCircle2.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'
+import './styles/bopae-circle2.css'
+
+const PART_WIDTH = 128
+const HOVER_DELTA_PX = 16
+
+function dir2rad(dir) {
+	return (dir-3)/4*Math.PI
+}
+
+function dir2deg(dir) {
+	return (dir-3)/4*180
+}
+
+function centerOffset(dir) {
+	let w = PART_WIDTH
+	return [[w/2,w], [0,w], [0,w/2], [0,0], [w/2,0], [w,0], [w,w/2], [w,w]][dir-1]
+}
+
+class BopaePattern extends Component {
+	render() {
+		let w = PART_WIDTH
+		let [x0, y0] = centerOffset(this.props.dir)
+		//let transform = `translate(${x0}, ${y0}) rotate(${dir2deg(this.props.dir)} 0 0)`
+		let img_src = `${process.env.PUBLIC_URL}/img/${this.props.name}.png`
+		return (
+			<pattern id="img1" patternUnits="userSpaceOnUse" width="256" height="256">
+				<image href={img_src} x={-w+x0} y={-w+y0} width="256" height="256" />
+			</pattern>
+		)
+	}
+}
+
+class BopaePart extends Component {
+	points(dir) {
+		let w = PART_WIDTH, da = Math.PI/8, dw = 1.05
+		let angle = dir2rad(dir)
+		let [x0, y0] = [w, w]
+		let [x1, y1] = [x0+Math.cos(angle-da)*w*dw+0.5|0, y0+Math.sin(angle-da)*w*dw+0.5|0]
+		let [x2, y2] = [x0+Math.cos(angle+da)*w*dw+0.5|0, y0+Math.sin(angle+da)*w*dw+0.5|0]
+		return `${x0},${y0} ${x1},${y1} ${x2},${y2}`
+	}
+	render() {
+		let angle = dir2rad(this.props.dir)
+		//let [x0, y0] = centerOffset(this.props.dir)
+		let [hoverDX, hoverDY] = [Math.cos(angle)*HOVER_DELTA_PX+0.5|0, Math.sin(angle)*HOVER_DELTA_PX+0.5|0]
+		let transform = `translate(${hoverDX}px, ${hoverDY}px)`
+		let img_src = `${process.env.PUBLIC_URL}/img/${this.props.name}.png`
+		let id = `bopae-pattern-${this.props.name}-${this.props.dir}`
+		return (
+			<g transform={`translate(${HOVER_DELTA_PX} ${HOVER_DELTA_PX})`}>
+				<pattern id={id} patternUnits="userSpaceOnUse" width="256" height="256">
+					<image xlinkHref={img_src} width="256" height="256" />
+				</pattern>
+				<polygon points={this.points(this.props.dir)} fill={'url(#'+id+')'} stroke={'url(#'+id+')'} style={{transform}} />
+			</g>
+		)
+	}
+}
+
+class BopaeCircle2 extends Component {
+	render() {
+		let w = PART_WIDTH*2 + HOVER_DELTA_PX*2
+		let dirs = [1,2,3,4,5,6,7,8]
+		return (
+			<svg className="bopae-circle2" width={w} height={w}>
+				{[].map((dir) =>
+					<BopaePattern key={dir} dir={dir} name={this.props.name} />
+				)}
+				{dirs.map((dir) =>
+					<BopaePart key={dir} dir={dir} name={this.props.name} onClick={(e) => this.props.onClick(e, dir)} />
+				)}
+			</svg>
+		)
+	}
+}
+
+export default BopaeCircle2
--- bopae_react/src/index.js
+++ bopae_react/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import BopaeList from './BopaeList'
 import BopaeCircle from './BopaeCircle'
+import BopaeCircle2 from './BopaeCircle2'
 import './styles/index.css'
 import bopaeDB from './bopae.json'
 
@@ -10,6 +11,7 @@ ReactDOM.render(
 	<div>
 		<BopaeList db={bopaeDB}/>
 		<BopaeCircle name={bopaeDB[0].name} onClick={(e, dir) => alert('clicked on #'+dir)}/>
+		<BopaeCircle2 name={bopaeDB[0].name} onClick={(e, dir) => alert('clicked on #'+dir)}/>
 	</div>,
 	document.getElementById('root')
 )
