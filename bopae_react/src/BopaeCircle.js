e61ad2a7857655db41b6af3758a59f2ec6e9af8b
3bl3gamer <3bl3gamer@gmail.com>
2016-10-28 14:10:00 +0300

    More simple layout generation

--- bopae_react/src/BopaeCircle.js
+++ bopae_react/src/BopaeCircle.js
@@ -4,32 +4,42 @@ import './styles/bopae-circle.css'
 const PART_WIDTH = 128
 const HOVER_DELTA_PX = 16
 
-class BopaePart extends Component {
-	dir2angle(dir) {
-		return (dir-3)/4*Math.PI
-	}
-	centerOffset(dir) {
-		let w = PART_WIDTH
-		return [null, [w/2,w], [0,w], [0,w/2], [0,0], [w/2,0], [w,0], [w,w/2], [w,w]][dir]
-	}
-	points(dir) {
-		let w = PART_WIDTH, da = Math.PI/8, dw = 1.05
-		let angle = this.dir2angle(dir)
-		let [x0, y0] = this.centerOffset(dir)
-		let [x1, y1] = [x0+Math.cos(angle-da)*w*dw+0.5|0, y0+Math.sin(angle-da)*w*dw+0.5|0]
-		let [x2, y2] = [x0+Math.cos(angle+da)*w*dw+0.5|0, y0+Math.sin(angle+da)*w*dw+0.5|0]
-		return `${x0},${y0} ${x1},${y1} ${x2},${y2}`
-	}
+function dir2rad(dir) {
+	return (dir-3)/4*Math.PI
+}
+
+function dir2deg(dir) {
+	return (dir-3)/4*180
+}
+
+function centerOffset(dir) {
+	let w = PART_WIDTH
+	return [[w/2,w], [0,w], [0,w/2], [0,0], [w/2,0], [w,0], [w,w/2], [w,w]][dir-1]
+}
+
+class BopaePartMask extends Component {
 	render() {
-		let w = PART_WIDTH, angle = this.dir2angle(this.props.dir)
-		let [x0, y0] = this.centerOffset(this.props.dir)
+		let [x0, y0] = centerOffset(this.props.dir)
+		let transform = `translate(${x0}, ${y0}) rotate(${dir2deg(this.props.dir)} 0 0)`
+		return (
+			<svg className="bopae-part-mask" style={{width: PART_WIDTH, height: PART_WIDTH}}>
+				<polygon points="0,0 124,-51 124,51" transform={transform} />
+			</svg>
+		)
+	}
+}
+
+class BopaePart extends Component {
+	render() {
+		let w = PART_WIDTH, angle = dir2rad(this.props.dir)
+		let [x0, y0] = centerOffset(this.props.dir)
 		let [hoverDX, hoverDY] = [Math.cos(angle)*HOVER_DELTA_PX, Math.sin(angle)*HOVER_DELTA_PX]
 		let transform = `translate(${hoverDX}px,${hoverDY}px)`
 		return (
-			<svg className="bopae-part" style={{left: w-x0, top: w-y0, width: w, height: w, transform}} onClick={this.props.onClick}>
-				<polygon points={this.points(this.props.dir)} />
-				<image href={process.env.PUBLIC_URL +'/img/'+ this.props.name + this.props.dir +'.png'} />
-			</svg>
+			<div className="bopae-part" style={{left: w-x0, top: w-y0, width: w, height: w}} onClick={this.props.onClick}>
+				<BopaePartMask dir={this.props.dir} />
+				<img style={{transform}} alt={'bopae-'+this.props.dir} src={process.env.PUBLIC_URL +'/img/'+ this.props.name + this.props.dir +'.png'} />
+			</div>
 		)
 	}
 }
@@ -38,8 +48,8 @@ class BopaeCircle extends Component {
 	render() {
 		return (
 			<div className="bopae-circle" style={{width: PART_WIDTH*2, height: PART_WIDTH*2}}>
-				{Array(8).fill(0).map((x,i) =>
-					<BopaePart key={i} dir={i+1} name={this.props.name} onClick={(e) => this.props.onClick(e, i+1)} />
+				{[1,2,3,4,5,6,7,8].map((dir) =>
+					<BopaePart key={dir} dir={dir} name={this.props.name} onClick={(e) => this.props.onClick(e, dir)} />
 				)}
 			</div>
 		)
