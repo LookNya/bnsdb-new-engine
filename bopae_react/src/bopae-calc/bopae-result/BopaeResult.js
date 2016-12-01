bd848316d6398442ba5c6f81583ff3f95c2d553d
LookNya <pavelx58@yandex.ru>
2016-12-01 18:13:25 +0300

    основа четвертого

--- bopae_react/src/bopae-calc/bopae-result/BopaeResult.js
+++ bopae_react/src/bopae-calc/bopae-result/BopaeResult.js
@@ -1,10 +1,27 @@
 import React, { PureComponent } from 'react'
+import BopaeSets from '../../global/bopae-sets/BopaeSets'
 import './bopae-result.css'
 
 class BopaeResult extends PureComponent {
+	// <BopaeSets bopae={this.props.selectedBopae} piecesCount={this.props.selectedBopaePiecesCount}/>
 	render(){
 		return (
-			<div className="bopae-result">
+			<div className="bopae-result margin-left">
+				<h2>Суммарно</h2>
+				<h3>Бонусы сетов</h3>
+				<div className="tip">Нет активных сетов</div>
+				<table>
+					<tr className="stat-names">
+						<td>Здоровье</td>
+						<td className="separator"></td>
+						<td>Защита</td>
+					</tr>
+					<tr className="stat-values">
+						<td>100 <span className="minus">120</span></td>
+						<td className="separator"></td>
+						<td>100 <span className="plus">120</span></td>
+					</tr>
+				</table>
 			</div>
 		)
 	}
